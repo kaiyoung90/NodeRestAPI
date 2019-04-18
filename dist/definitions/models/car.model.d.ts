e32fa4de1b86513026d8780a68b248a39d2d4de1
Kai Young <kaiyoung90@mail.fresnostate.edu>
2019-04-18 11:48:04 -0700

    finished get endpoints for temp car model

--- dist/definitions/models/car.model.d.ts
+++ dist/definitions/models/car.model.d.ts
@@ -1,5 +1,8 @@
+import { Request, Response, NextFunction } from 'express';
 export declare class Car {
     _model: any;
     constructor(norm: any);
+    getAllCars(model: any): (req: Request, res: Response, next: NextFunction) => Promise<void>;
+    getCarByID(model: any): (req: Request, res: Response, next: NextFunction) => Promise<void>;
     model: any;
 }
